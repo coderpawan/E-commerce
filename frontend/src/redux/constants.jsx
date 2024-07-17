0e17f7c792d189f6c00c8951d94edcfdb2a8e0e0
PAWAN KUMAR SAHU <89339624+coderpawan@users.noreply.github.com>
2024-07-17 14:48:09 +0530

    updated

--- frontend/src/redux/constants.jsx
+++ frontend/src/redux/constants.jsx
@@ -1,7 +1,13 @@
-export const BASE_URL = "";
-export const USERS_URL = "/api/users";
-export const CATEGORY_URL = "/api/category";
-export const PRODUCT_URL = "/api/products";
-export const UPLOAD_URL = "/api/upload";
-export const ORDERS_URL = "/api/orders";
-export const PAYPAL_URL = "/api/config/paypal";
+export const BASE_URL = "https://e-commerce-backend-five-jade.vercel.app";
+export const USERS_URL =
+  "https://e-commerce-backend-five-jade.vercel.app/api/users";
+export const CATEGORY_URL =
+  "https://e-commerce-backend-five-jade.vercel.app/api/category";
+export const PRODUCT_URL =
+  "https://e-commerce-backend-five-jade.vercel.app/api/products";
+export const UPLOAD_URL =
+  "https://e-commerce-backend-five-jade.vercel.app/api/upload";
+export const ORDERS_URL =
+  "https://e-commerce-backend-five-jade.vercel.app/api/orders";
+export const PAYPAL_URL =
+  "https://e-commerce-backend-five-jade.vercel.app/api/config/paypal";
