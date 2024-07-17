af238fa5fa32a95d7e7b69b9ac3ab7a8959d019e
PAWAN KUMAR SAHU <89339624+coderpawan@users.noreply.github.com>
2024-07-17 14:24:17 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+const cors = require("cors");
 
 // Utiles
 import connectDB from "./config/db.js";
@@ -23,18 +24,36 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(function (req, res, next) {
+  let allowedOrigins = ["*"]; // list of url-s
+  let origin = req.headers.origin;
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Expose-Headers", "Content-Disposition");
+  next();
+});
+app.use(cors());
+
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/orders", orderRoutes);
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
 app.get("/api/config/paypal", (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
 
 const __dirname = path.resolve();
-console.log(__dirname);
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
