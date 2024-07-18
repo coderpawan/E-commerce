8e37d54339012fddd13bbdf7d48c4c8b6a823c1a
PAWAN KUMAR SAHU <89339624+coderpawan@users.noreply.github.com>
2024-07-18 17:25:27 +0530

    Update createToken.js

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -7,10 +7,11 @@ const generateToken = (res, userId) => {
 
   // Set JWT as an HTTP-Only Cookie
   res.cookie("jwt", token, {
-    httpOnly: false,
+    httpOnly: true,
     secure: true,
     maxAge: 1000 * 60 * 60 * 48,
     sameSite: "none",
+    domain: ".vercel.app"
   });
 
   return token;
